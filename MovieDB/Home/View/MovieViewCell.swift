81042669d0cd9c0f7a223de7d0f321565da0557b
Stefanny Aquino <stefanny.aquino.gutierrez@gmail.com>
2022-12-28 12:23:52 -0500

    fix movie cell view

--- MovieDB/Home/View/MovieViewCell.swift
+++ MovieDB/Home/View/MovieViewCell.swift
@@ -13,47 +13,56 @@ struct MovieViewCell: View {
     var tvShow: TVShow
     let selectedMovie : () -> Void
     
+    var description: String {
+        tvShow.description.isEmpty ? "No description found" : tvShow.description
+    }
+    
     var body: some View {
         VStack(spacing: 0) {
             ImageView(url: Helper.getImageUrl(tvShow.imagePath))
                 .scaledToFill()
-                .frame(height: 230)
-            VStack {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text(tvShow.name)
-                        .bold()
-                        .font(.footnote)
-                        .lineLimit(1)
-                    HStack {
-                        Text(tvShow.airDate.formatDate())
-                            .bold()
-                            .font(.caption2)
-                        Spacer()
-                        HStack(spacing: 3) {
-                            Image(systemName: "star.fill")
-                                .resizable()
-                                .frame(width: 10, height: 10)
-                            Text("\(tvShow.vote.format())")
-                                .bold()
-                                .font(.caption2)
-                        }
-                    }
-                    Text(tvShow.description)
-                        .foregroundColor(.white)
-                        .font(.caption2)
-                        .lineLimit(4)
-                }
-                .padding(10)
-                .padding(.bottom, 12)
-                .foregroundColor(Color("primary.green"))
-            }
-            .frame(height: 120)
-            .background(Color("black"))
+                .frame(height: 220)
+            infoView
         }
         .cornerRadius(15)
         .onTapGesture {
             selectedMovie()
         }
+    }
+
+    var infoView: some View {
+        VStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(tvShow.name)
+                    .bold()
+                    .font(.footnote)
+                    .lineLimit(1)
+                HStack {
+                    Text(tvShow.airDate.formatDate())
+                        .bold()
+                        .font(.caption2)
+                    Spacer()
+                    HStack(spacing: 3) {
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 10, height: 10)
+                        Text("\(tvShow.vote.format())")
+                            .bold()
+                            .font(.caption2)
+                    }
+                }
+                Text(description)
+                    .foregroundColor(.white)
+                    .font(.caption2)
+                    .lineLimit(4)
+                    .frame(height: 56)
+            }
+            .padding(10)
+            .padding(.bottom, 5)
+            .foregroundColor(Color("primary.green"))
+        }
+        .frame(height: 130)
+        .background(Color("black"))
     }
 }
 
